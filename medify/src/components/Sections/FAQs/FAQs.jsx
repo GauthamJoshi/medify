e03e756eeb2ea85bd05feea3e6681bfea6b4e580
gauthamjoshi999@gmail.com <gauthamjoshi999@gmail.com>
2025-04-17 18:30:32 +0530

    final commit

--- medify/src/components/Sections/FAQs/FAQs.jsx
+++ medify/src/components/Sections/FAQs/FAQs.jsx
@@ -1,6 +1,6 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
 import faqBanner from '../../../assets/faqs-banner.jpg'
-import CustomizedAccordions from "../../Accordion/Accordion";
+import CustomizedAccordions from "../../Accordian/Accordian";
 
 export default function FAQs() {
 
